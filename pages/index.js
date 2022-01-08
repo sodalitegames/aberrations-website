efde06da1ce9b0fdc6d8bbd2d6b766bc98251e5a
Talon Durrant <talonmdurrant@gmail.com>
2022-01-08 15:17:21 -0500

    setting up netlify cms quick way

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,11 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
+import { attributes, react as HomeContent } from '../content/home.md';
+
 export default function Home() {
+  let { title, cats } = attributes;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -21,6 +25,19 @@ export default function Home() {
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
+
+        <article>
+          <h1>{title}</h1>
+          <HomeContent />
+          <ul>
+            {cats.map((cat, k) => (
+              <li key={k}>
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
