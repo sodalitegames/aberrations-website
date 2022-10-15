59900f8d83412209d9c85c992fdccd49e6f95ffb
Talon Durrant <talonmdurrant@gmail.com>
2022-10-15 05:45:17 -0600

    update usable card to include units

--- components/elements/cards/usable-card.js
+++ components/elements/cards/usable-card.js
@@ -4,10 +4,14 @@ import CardContainer from '../card-container';
 export default function UsableCard({ usable }) {
   return (
     <CardContainer heading={usable.name}>
-      <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+      <dl className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-2">
         <div className="sm:col-span-1">
           <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">Type</dt>
           <dd className="mt-1 text-sm">{usable.type.replaceAll('_', ' ')}</dd>
+        </div>
+        <div className="sm:col-span-1">
+          <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">Units</dt>
+          <dd className="mt-1 text-sm">{usable.units}</dd>
         </div>
         <div className="sm:col-span-2">
           <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">Description</dt>
