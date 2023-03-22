946ffa0cd880b3fa9e9252be31f224353d1ca64d
Talon Durrant <talonmdurrant@gmail.com>
2023-03-22 01:36:14 -0600

    authenticate and login magic link verification

--- pages/auth/authenticate.js
+++ pages/auth/authenticate.js
@@ -1,22 +1,35 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { useStytch, useStytchSession } from '@stytch/nextjs';
+import { useEffect, useState } from 'react';
+import { useStytch } from '@stytch/nextjs';
+
+import { session_duration_minutes } from '../../lib/stytch';
+import Loader from '../../components/dashboard/components/Loader';
 
 export default function Authenticate() {
   const router = useRouter();
   const stytch = useStytch();
-  const { session } = useStytchSession();
+
+  const [resp, setResp] = useState(null);
 
   useEffect(() => {
-    if (session) {
-      router.push('/dashboard');
-    } else {
+    (async () => {
       const { token } = router.query;
-      stytch.magicLinks.authenticate(token, {
-        session_duration_minutes: 60,
-      });
-    }
-  }, [stytch, session, router]);
 
-  return <div>Loading</div>;
+      if (token) {
+        const resp = await stytch.magicLinks.authenticate(token, {
+          session_duration_minutes,
+        });
+
+        setResp(resp);
+      }
+    })();
+  }, [stytch, router]);
+
+  console.log(resp);
+
+  if (resp) {
+    return JSON.stringify(resp);
+  }
+
+  return <Loader />;
 }
