fff070c87c3aa41e42b6e12cdb1e703b0c813e24
Talon Durrant <talonmdurrant@gmail.com>
2023-03-22 02:15:10 -0600

    set up account-settings to use new stytch auth

--- components/dashboard/dashboard-pages/account-settings.js
+++ components/dashboard/dashboard-pages/account-settings.js
@@ -1,4 +1,7 @@
-import { useState, useEffect } from 'react';
+import { Fragment, useState, useCallback } from 'react';
+import { useStytch } from '@stytch/nextjs';
+
+import { login_expiration_minutes, session_duration_minutes } from '../../../lib/stytch';
 
 import { ExclamationCircleIcon, CheckCircleIcon } from '@heroicons/react/solid';
 
@@ -6,20 +9,18 @@ import classNames from '../../../utils/functions/classnames';
 
 import FormSection from '../components/FormSection';
 
-import { useAuth } from '../../../contexts/auth';
 import Notice from '../../elements/notice';
 
 export default function Account({ user }) {
-  const { setUser, updateUser, updateUserEmail, updateUserPassword, sendVerificationEmail, verifyEmailStart } = useAuth();
+  const stytch = useStytch();
 
   // account settings
   const [firstName, setFirstName] = useState(user.name.first_name);
   const [lastName, setLastName] = useState(user.name.last_name);
 
   // email change
-  const [email, setEmail] = useState(user.email);
+  const [email, setEmail] = useState('');
   const [emailSent, setEmailSent] = useState(false);
-  const [emailVerified, setEmailVerified] = useState(true);
 
   // password change
   const [passwordCurrent, setPasswordCurrent] = useState('');
@@ -31,41 +32,50 @@ export default function Account({ user }) {
   const [emailMessage, setEmailMessage] = useState(null);
   const [passwordMessage, setPasswordMessage] = useState(null);
   const [processing, setProcessing] = useState(null);
-  const [notice, setNotice] = useState(null);
 
-  useEffect(() => {
-    // Check to see if this is a reload from changing their password
-    const query = new URLSearchParams(window.location.search);
-    if (query.get('password') === 'success') {
-      setNotice({ status: 'success', message: 'Your password has been successfully updated.' });
-    }
-  }, []);
+  const updateUser = useCallback(
+    async data => {
+      return await stytch.user.update(data);
+    },
+    [stytch]
+  );
 
-  useEffect(() => {
-    // set verify message if user's email has been verified
-    if (!user.isEmailVerified) {
-      setEmailVerified(false);
-    }
-  }, [user.isEmailVerified]);
+  const resetPassword = useCallback(
+    async (existing_password, new_password) => {
+      return await stytch.passwords.resetByExistingPassword({
+        existing_password,
+        new_password,
+        session_duration_minutes,
+      });
+    },
+    [stytch]
+  );
+
+  const sendMagicLink = useCallback(
+    async email => {
+      return await stytch.magicLinks.email.send(email, {
+        login_magic_link_url: 'http://localhost:3000/auth/verify-email',
+        signup_magic_link_url: 'http://localhost:3000/auth/verify-email',
+        login_expiration_minutes,
+      });
+    },
+    [stytch]
+  );
 
   const sendEmailVerification = async email => {
-    const resp = await verifyEmailStart(email);
+    const resp = await sendMagicLink(email);
 
     console.log('HERE', resp);
 
-    setEmailSent(resp);
-
-    // if (message.status === 'success') {
-    //   setEmailSent({ status: 'success', message: 'Verification email has been sent. Please check your email and follow the instructions on the email.' });
-    // } else {
-    //   setEmailSent(message);
-    // }
+    if (resp) {
+      setEmailSent({ status: 'success', message: 'Verification email has been sent. Please check your onbox and follow the instructions on the email.' });
+    }
   };
 
-  const updateAccountDetailsHandler = async e => {
+  const updateUserHandler = async e => {
     e.preventDefault();
 
-    setProcessing('account-details');
+    setProcessing('update-user');
 
     if (!firstName) {
       setSettingsMessage({ status: 'error', message: 'You must provide a first name.' });
@@ -79,21 +89,22 @@ export default function Account({ user }) {
       return;
     }
 
-    const data = await updateUser({ name: { first_name: firstName, last_name: lastName } });
+    const resp = await updateUser({ name: { first_name: firstName, last_name: lastName } });
 
-    if (data) {
-      setSettingsMessage({ status: data.status, message: data.message });
-      setFirstName(data.data.user.name.first_name);
-      setLastName(data.data.user.name.last_name);
-      setUser(data.data.user);
+    console.log(resp);
+
+    if (resp) {
+      setSettingsMessage({ status: 'success', message: 'Updates successfully applied.' });
+      setFirstName(resp.user.name.first_name);
+      setLastName(resp.user.name.last_name);
       setProcessing(false);
     }
   };
 
-  const updateEmailHandler = async e => {
+  const addEmailHandler = async e => {
     e.preventDefault();
 
-    setProcessing('email-change');
+    setProcessing('add-email');
 
     if (!email) {
       setEmailMessage({ status: 'error', message: 'You must provide an email.' });
@@ -101,29 +112,20 @@ export default function Account({ user }) {
       return;
     }
 
-    if (email === user.email) {
-      setEmailMessage({ status: 'info', message: 'There are no changes to apply.' });
-      setProcessing(false);
-      return;
-    }
+    const resp = await sendMagicLink(email);
 
-    const data = await updateUserEmail(user.email, email);
+    console.log(resp);
 
-    if (data) {
-      setEmailMessage({ status: data.status, message: data.message });
-
-      if (data.stats === 'success') {
-        setEmail(user.email);
-      }
-
+    if (resp) {
+      setEmailMessage({ status: 'success', message: 'Please check your inbox to verify your new email address.' });
       setProcessing(false);
     }
   };
 
-  const updatePasswordHandler = async e => {
+  const resetPasswordHandler = async e => {
     e.preventDefault();
 
-    setProcessing('password-change');
+    setProcessing('reset-password');
 
     if (!passwordCurrent) {
       setPasswordMessage({ status: 'error', message: 'You must provide your current password.' });
@@ -149,25 +151,23 @@ export default function Account({ user }) {
       return;
     }
 
-    const message = await updateUserPassword(passwordCurrent, password, passwordConfirm);
+    const resp = await resetPassword(passwordCurrent, password);
 
-    if (message) {
-      setPasswordMessage(message);
+    if (resp) {
+      setPasswordMessage({ status: 'success', message: 'Password successfully reset.' });
       setProcessing(false);
     }
   };
 
   return (
     <>
-      {notice ? <Notice status={notice.status} message={notice.message} hideable /> : null}
-
       <FormSection
         heading="Account Details"
         description="Update your account information."
-        ariaTag="account-details"
+        ariaTag="update-user"
         submitText="Save changes"
         submitDescription="Please enter your full name, or a display name you are comfortable with."
-        submitHandler={updateAccountDetailsHandler}
+        submitHandler={updateUserHandler}
         processing={!!(processing === 'account-details')}
       >
         <>
@@ -210,52 +210,56 @@ export default function Account({ user }) {
         ariaTag="your-email"
         submitText="Save changes"
         submitDescription="We will email you to verify the change."
-        submitHandler={updateEmailHandler}
-        processing={!!(processing === 'email-change')}
+        submitHandler={addEmailHandler}
+        processing={!!(processing === 'add-email')}
       >
         <>
-          {user.emails.map(({ email, verified }) => (
-            <>
-              <div className="my-3 md:w-6/12">
-                <label htmlFor="email-address" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
-                  Email address
-                </label>
-
+          <div className="my-3 md:w-6/12">
+            <label htmlFor="email-address" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+              Email address
+            </label>
+            {user.emails.map(({ email, verified }) => (
+              <Fragment key={email}>
                 <div className="relative mt-1 rounded-md shadow-sm">
-                  <input
-                    type="email"
-                    name="email-address"
-                    id="email-address"
-                    value={email}
-                    autoComplete="email"
-                    className="w-full border border-gray-300 shadow-sm input-secondary dark:border-transparent"
-                  />
+                  <input type="text" className="w-full border border-gray-300 shadow-sm input-secondary dark:border-transparent" value={email} disabled />
                   <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
                     {verified ? <CheckCircleIcon className="w-5 h-5 text-green-500" aria-hidden="true" /> : <ExclamationCircleIcon className="w-5 h-5 text-red-500" aria-hidden="true" />}
                   </div>
                 </div>
-              </div>
 
-              {/* If email is verified */}
-              {verified ? (
-                <p className="mt-2 text-sm text-green-600" id="verify-message">
-                  Your email is verified.
-                </p>
-              ) : (
-                <p className="mt-2 text-sm text-red-600" id="verify-message">
-                  {emailSent && <p className={classNames('font-medium', emailSent.status === 'success' ? 'text-green-600' : 'text-red-600')}>{emailSent.message}</p>}
-                  {!emailSent && (
-                    <span className="font-medium text-red-600">
-                      Your email is not verified. If you have not recieved a verfication email, you can{' '}
-                      <span className="underline cursor-pointer hover:text-red-700" onClick={() => sendEmailVerification(email)}>
-                        send another one here.
+                {/* If email is verified */}
+                {verified ? (
+                  <p className="my-2 text-sm text-green-600" id="verify-message">
+                    Your email is verified.
+                  </p>
+                ) : (
+                  <p className="my-2 text-sm text-red-600" id="verify-message">
+                    {emailSent && <span className={classNames('font-medium', emailSent.status === 'success' ? 'text-green-600' : 'text-red-600')}>{emailSent.message}</span>}
+                    {!emailSent && (
+                      <span className="font-medium text-red-600">
+                        Your email is not verified. If you have not recieved a verfication email, you can{' '}
+                        <a className="underline cursor-pointer hover:text-red-700" onClick={() => sendEmailVerification(email)}>
+                          send another one here.
+                        </a>
                       </span>
-                    </span>
-                  )}
-                </p>
-              )}
-            </>
-          ))}
+                    )}
+                  </p>
+                )}
+              </Fragment>
+            ))}
+            <button className="mt-4 btn-tertiary">Add New Email</button>
+            <div className="relative mt-1 rounded-md shadow-sm">
+              <input
+                type="email"
+                name="email-address"
+                id="email-address"
+                value={email}
+                autoComplete="email"
+                className="w-full border border-gray-300 shadow-sm input-secondary dark:border-transparent"
+                onChange={e => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
 
           {/* Email change feedback */}
           {emailMessage ? <Notice status={emailMessage.status} message={emailMessage.message} hideable /> : null}
@@ -268,8 +272,8 @@ export default function Account({ user }) {
         ariaTag="change-password"
         submitText="Save changes"
         submitDescription="For your security, you must enter your current password first in order to change it."
-        submitHandler={updatePasswordHandler}
-        processing={!!(processing === 'password-change')}
+        submitHandler={resetPasswordHandler}
+        processing={!!(processing === 'reset-password')}
       >
         <>
           <div className="my-3 md:w-6/12">
