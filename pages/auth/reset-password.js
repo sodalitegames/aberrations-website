c22eb3632caedbe0c4d69afc8495bcb99fba5a11
Talon Durrant <talonmdurrant@gmail.com>
2023-03-22 02:51:52 -0600

    styled reset-password page

--- pages/auth/reset-password.js
+++ pages/auth/reset-password.js
@@ -1,11 +1,21 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { Products } from '@stytch/vanilla-js';
-import { StytchPasswordReset } from '@stytch/nextjs';
+import { StytchPasswordReset, useStytchSession } from '@stytch/nextjs';
 
 import { login_expiration_minutes, reset_password_expiration_minutes } from '../../lib/stytch';
 
-export default function ResetPassword() {
+import PageLayout from '../../layouts/PageLayout';
+
+export default function ResetPassword({ metadata }) {
   const router = useRouter();
+  const { session } = useStytchSession();
+
+  useEffect(() => {
+    if (session) {
+      router.push('/dashboard');
+    }
+  }, [session, router]);
 
   const { token } = router.query;
 
@@ -13,15 +23,50 @@ export default function ResetPassword() {
 
   console.log('HELLO');
 
-  const config = {
-    passwordOptions: {
-      loginExpirationMinutes: login_expiration_minutes,
-      loginRedirectURL: 'http://localhost:3000/auth/authenticate',
-      resetPasswordExpirationMinutes: reset_password_expiration_minutes,
-      resetPasswordRedirectURL: 'http://localhost:3000/auth/reset-password',
+  const props = {
+    config: {
+      passwordOptions: {
+        loginExpirationMinutes: login_expiration_minutes,
+        loginRedirectURL: 'http://localhost:3000/auth/authenticate',
+        resetPasswordExpirationMinutes: reset_password_expiration_minutes,
+        resetPasswordRedirectURL: 'http://localhost:3000/auth/reset-password',
+      },
+      products: [Products.passwords],
     },
-    products: [Products.passwords],
+    styles: {
+      fontFamily: '"Kanit", sans-serif',
+      container: {
+        width: '100%',
+        borderColor: 'transparent',
+        backgroundColor: 'transparent',
+      },
+      buttons: {
+        primary: {
+          backgroundColor: '#164650',
+        },
+        secondary: {
+          backgroundColor: '#001D3D',
+        },
+      },
+    },
   };
 
-  return <StytchPasswordReset config={config} passwordResetToken={token} />;
+  return (
+    <PageLayout title={metadata.title} seo={metadata} full>
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <StytchPasswordReset config={props.config} styles={props.styles} passwordResetToken={token} />
+      </div>
+    </PageLayout>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      metadata: {
+        title: 'Reset password',
+        slug: '/auth/reset-password',
+      },
+    },
+  };
 }
